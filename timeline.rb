51c8242247bada3edd455b6c4b1faef1e196c09d
SORA0920 <sora02230112@gmail.com>
2018-03-15 23:14:03 +0900

    Listの表示とかに対応

--- timeline.rb
+++ timeline.rb
@@ -6,21 +6,95 @@ require "thread"
 require "nokogiri"
 require "optparse"
 
-def timeline_create(account, tl, tl_name)
-  uri = URI.parse("https://#{host}/api/v1/timelines/#{tl}limit=40")
+def load_account(file)
+  begin
+    file = File.open(file, "a+")
+  rescue
+    puts "Error"
+    exit 1
+  end
+
+  file_str = []
+  file.each_line do |line|
+    file_str.push(line.chop)
+  end
+
+  file_str = file_str.join("\n")
+  
+  file.close
+
+  begin 
+    ac = JSON.parse(file_str)
+  rescue
+    puts "Parse Error"
+    exit 1
+  end
+  return ac
+end
+
+def timeline_load(account, tl, limit)
+  uri = URI.parse("https://#{account["host"]}/api/v1/timelines/#{tl}limit=#{limit}")
 
   https = Net::HTTP.new(uri.host, uri.port)
   https.use_ssl = true
 
   req = Net::HTTP::Get.new(uri.path)
-  req["Authorization"] = token
+  req["Authorization"] = " Bearer " + account["token"]
 
   res = https.request(req)
 
-  toots = JSON.parse(res.body)
+  puts res.code
+  puts res.body
+  return toots = JSON.parse(res.body)
+end
 
-  toots.each{ |toot|
-    Plugin.call :extract_receive_message, tl_name, create_toot(toot)
+def listlist(account)
+  uri = URI.parse("https://#{account["host"]}/api/v1/lists")
+
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+
+  req = Net::HTTP::Get.new(uri.path)
+  req["Authorization"] = " Bearer " + account["token"]
+
+  res = https.request(req)
+
+  lists = JSON.parse(res.body)
+
+  puts "ID  Title\n\n"
+  lists.each{ |list| 
+    li = list
+    puts "#{li["id"]}  #{li["title"]}"  
   }
 end
+
+def timeline_parse(json)
+  puts "未実装" 
+end
+
+account = load_account("account.json")
+tl = "home?"
+limit = `tput lines`
+stream = false
+mediaonly = false
+
+OptionParser.new do |opt|
+  opt.on('--home',            'Display home timeline'                      ) { tl = "home?" }
+  opt.on('--local',           'Display local timeline'                     ) { tl = "?local=true&" }
+  opt.on('--public',          'Display public timeline'                    ) { tl = "public?" }
+  opt.on('--stream',          'Start up in streaming mode'                 ) { stream = true }
+  opt.on('--mediaonly',       'Retrieve only posts that include media'     ) { mediaonly = true }
+  opt.on('--list [ID]',       'Display list timeline'                      ) { |id| tl = "list/#{id}?" }
+  opt.on('--limit [1-40]',    'Specify the number of Toot to acquire'      ) { |lim| limit = lim }
+  opt.on('--lists',           'Retrieving lists'                           ) { 
+                                                                               listlist(account) 
+                                                                               exit 0
+                                                                             }
+  
+  opt.parse!(ARGV)
+end
+
+
+
+puts timeline_load(account, tl, limit)
 
