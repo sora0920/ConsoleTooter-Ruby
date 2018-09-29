44749142ffe0cac1053d1399344187dfb82bca51
SORA0920 <sora02230112@gmail.com>
2018-09-29 14:24:11 +0900

    Update console.rb

--- console.rb
+++ console.rb
@@ -1,13 +1,16 @@
 require "curses"
+require_relative "./timeline.rb"
 
 Curses.init_screen
 begin
   s = "Hello World!"
   win = Curses::Window.new(Curses.lines / 1.8, Curses.cols, 1, 1)
-  Curses.setpos(Curses.lines / 2, Curses.cols / 2 - (s.length / 2))
-  Curses.addstr(s)
-  Curses.refresh
-  Curses.getch
+  #win.addstr(s)
+  stream(account, tl, param, img, safe)
+  loop do
+    win.refresh
+  end
+  win.close
 ensure
   Curses.close_screen
 end
