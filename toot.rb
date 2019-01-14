0fbcea4d773ed14de21dce796044160c129fe2fa
sora0920 <sora02230112@gmail.com>
2019-01-14 18:37:09 +0900

    clean toot.rb

--- toot.rb
+++ toot.rb
@@ -25,24 +25,24 @@ sen = false
 sd = false
 
 opts = {
-  "sensitive" = false,
-  "sd" = false,
-  "visibility" = "public", 
-  "spoiler_text" = "",
-  "in_reply_to_id" = "",
-  "media_ids" = []
+  "sensitive" => false,
+  "sd" => false,
+  "visibility" => "public", 
+  "spoiler_text" => "",
+  "in_reply_to_id" => "",
+  "media_ids" => []
 }
 
 OptionParser.new do |opt|
-  opt.on('--public',             'Set visibility to public'  ) { vis = "public" }
-  opt.on('--unlisted',           'Set visibility to unlisted') { vis = "unlisted" }
-  opt.on('--private',            'Set visibility to private' ) { vis = "private" }
-  opt.on('--direct',             'Set visibility to direct'  ) { vis = "direct" }
-  opt.on('--cw [TEXT]',          'Set CW TEXT=warning text'  ) { |cw_txt| cw = "#{cw_txt}" }
-  opt.on('--reply [Reply to ID]','Post reply'                ) { |re| reply_id = "#{re.to_i}"}
-  opt.on('--media [path]',       'Post with images'          ) { |media| media_id.push(postmedia(account, media))}
-  opt.on('--nsfw',               'Set NSFW flag'             ) { sen = true }
-  opt.on('--sd',                 'To "totsuzen no shi"'      ) { sd = true }
+  opt.on('--public',             'Set visibility to public'  ) { opts["visibility"] = "public" }
+  opt.on('--unlisted',           'Set visibility to unlisted') { opts["visibility"] = "unlisted" }
+  opt.on('--private',            'Set visibility to private' ) { opts["visibility"] = "private" }
+  opt.on('--direct',             'Set visibility to direct'  ) { opts["visibility"] = "direct" }
+  opt.on('--cw [TEXT]',          'Set CW TEXT=warning text'  ) { |cw_txt| opts["spoiler_text"] = "#{cw_txt}" }
+  opt.on('--reply [Reply to ID]','Post reply'                ) { |re| opts["in_reply_to_id"] = "#{re.to_i}"}
+  opt.on('--media [path]',       'Post with images'          ) { |media| opts["media_ids"].push(postmedia(account, media))}
+  opt.on('--nsfw',               'Set NSFW flag'             ) { opts["sensitive"] = true }
+  opt.on('--sd',                 'To "totsuzen no shi"'      ) { opts["sd"] = true }
 
   opt.parse!(ARGV)
 end
@@ -53,10 +53,10 @@ if ARGV[0].nil? || ARGV[0].empty? then
   exit!
 end
 
-if sd
+if opts["sd"]
   body = to_suddenly_death(ARGV[0])
 else
   body = ARGV[0]
 end
 
-post_toot(vis, cw, account, body, reply_id, media_id, sen)
+post_toot(opts["visibility"], opts["spoiler_text"], account, body, opts["in_reply_to_id"], opts["media_ids"], opts["sensitive"])
